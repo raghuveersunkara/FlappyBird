eabd20af7f7b903082c8e0e5ec05910b79412a5d
raghuveersunkara <raghuveer.sunkara@gmail.com>
2019-12-31 14:12:25 -0600

    Added move method

--- bird.py
+++ bird.py
@@ -23,4 +23,22 @@ class Bird():
 
     def jump(self):
         self.velocity = -10.5
+        self.tick_count = 0
+        self.height = self.y
+
+    def move(self):
+        self.tick_count += 1
+        
+        displacement = self.velocity*self.tick_count + 1.5*self.tick_count**2
+
+        if displacement >= 16:
+            displacement = 16
+        
+        if displacement < 0:
+            displacement -= 2
+
+        self.y = self.y + displacement
+
+        if displacement < 0 or self.y < self.height + 50:
+            pass
         
